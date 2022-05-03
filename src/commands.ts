cd88930bce1965b2041884af07d6e0ad03c56ca2
t.hirashima <hira9603859504@gmail.com>
2022-05-03 13:24:27 +0900

    Update to use quickOpen

--- src/commands.ts
+++ src/commands.ts
@@ -1,26 +1,26 @@
 'use strict';
 import * as vs from 'vscode';
 
-type File = {
-    filePath: string;
-};
-
 export function openFile(githubUrl: string): void {
-    let file: File = parseGithubUrl(githubUrl);
-    // vscodeOpenFile(file);
-    vs.window.showInformationMessage(`Open ${file.filePath}`);
+    let quickOpenPrefix: string = parseGithubUrl(githubUrl);
+    vs.commands.executeCommand('workbench.action.quickOpen', quickOpenPrefix);
 }
 
-export function parseGithubUrl(githubUrl: string): File {
+export function parseGithubUrl(githubUrl: string): string {
     let re = /https:\/\/github\.com\/.+\/blob\/[^\/]+\/(.*)/;
     let matches = re.exec(githubUrl);
     if (matches === null) { throw new Error('Failed to parse github url: ' + githubUrl); }
 
     let filePath = matches[1].split('#')[0];
 
-    console.log('filePath: ' + filePath);
+    let lineRange = matches[1].split('#')[1];
+    if (lineRange) {
+        let lineRangeRe = /L(\d+)-L(\d+)/;
+        let lineRangeMatches = lineRangeRe.exec(lineRange);
+        if (lineRangeMatches === null) { throw new Error('Failed to parse line range: ' + lineRange); }
+        filePath += ':' + lineRangeMatches[1];
+    }
 
-    return {
-        filePath: filePath,
-    };
+    console.log('filePath: ' + filePath);
+    return filePath;
 }
--- src/test/suite/commands.test.ts
+++ src/test/suite/commands.test.ts
@@ -6,8 +6,8 @@ suite('Commands Test Suite', () => {
 	vscode.window.showInformationMessage('Start commands tests.');
 
 	test('parseGithubUrl', () => {
-		let url = 'https://github.com/user-name/repo-name/blob/branch/folder1/folder2/file.name#L1-L20';
-		let file = commands.parseGithubUrl(url);
-		assert.strictEqual(file.filePath, 'folder1/folder2/file.name');
+		let url = 'https://github.com/user-name/repo-name/blob/branch/folder1/folder2/file.name#L5-L20';
+		let quickOpenPrefix = commands.parseGithubUrl(url);
+		assert.strictEqual(quickOpenPrefix, 'folder1/folder2/file.name:5');
 	});
 });
